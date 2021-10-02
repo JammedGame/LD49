5d3788ca1775f332368799023c6f1d44e44bcb5e
Stefan Stegic <stefanst@nordeus.com>
2021-10-02 13:08:10 +0200

    Add rider metadata.

---
+++ Assets/Scripts/BattleSimulator/Spells/SpellSettings.cs
@@ -0,0 +1,13 @@
+﻿using Game.Simulation;
+using Unity.Mathematics;
+
+namespace BattleSimulator.Spells
+{
+    public class SpellSettings : BattleObjectSettings
+    {
+        public override BattleObject Spawn(GameWorld world, float2 position, OwnerId owner)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
