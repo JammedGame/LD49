299474bc9af1c70aa9e330fadc2fda083f475179
Stefan Stegic <stefanst@nordeus.com>
2021-10-03 21:00:20 +0200

    Import city fbx.

---
+++ Assets/Scripts/BattleSimulator/Spells/TortureWaveSpell.cs
@@ -0,0 +1,12 @@
+﻿using Game.Simulation;
+
+namespace BattleSimulator.Spells
+{
+    public class TortureWaveSpell : Spell
+    {
+        
+        public TortureWaveSpell(BattleObject caster, SpellSettings settings, GameWorld gameWorld) : base(caster, settings, gameWorld)
+        {
+        }
+    }
+}
---
+++ Assets/Scripts/BattleSimulator/Spells/TortureWaveSpellSettings.cs
@@ -0,0 +1,16 @@
+﻿using Game.Simulation;
+
+namespace BattleSimulator.Spells
+{
+    public class TortureWaveSpellSettings : SpellSettings
+    {
+        public float coneLength;
+        public float coneAngle;
+        public float coneTravelSeconds;
+        public float damage;
+        public override BattleObject Spawn(GameWorld world, UnitTargetInfo targetInfo, OwnerId ownerId, BattleObject parent = null)
+        {
+            return new TortureWaveSpell(parent, this, world);
+        }
+    }
+}
